cb2d0d071a5c297d1ec10ea209f85a8e2fb6f8d4
Zniper <me.zniper@gmail.com>
2016-04-17 01:28:06 +0700

    Refine the configuration files. Add Gunicorn starting script.

--- deploy/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-import multiprocessing
-
-bind = "unix:/opt/projects/{{ project_name }}/src/gunicorn.sock"
-workers = 4
-errorlog = "/opt/projects/{{ project_name }}/logs/gunicorn_error.log"
-loglevel = "error"
-proc_name = "{{ project_name }}"
